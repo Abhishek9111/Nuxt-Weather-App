3289f7db38d063576905b3c73066598ea14efe0b
Abhishek Kapoor <kapoorabhishek567@gmail.com>
2022-07-22 00:10:14 +0530

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-toolbar color="indigo" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Learning Nuxt</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>
+            <n-link to="/weather-app">Weather App</n-link>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <transition>
+        <v-content>
+          <nuxt />
+        </v-content>
+      </transition>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
---
+++ pages/weather-app.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <h1 class="display-1 text-xs-center">Weather App</h1>
+    <v-layout>
+      <v-flex xs12 class="mt-4">
+        <v-card-text>Enter the name of the city </v-card-text>
+      </v-flex>
+      <v-form @submit.prevent="getWeatherInfo()">
+        <v-flex xs12 class="mt-4">
+          <v-text-field
+            v-model="city"
+            label="Enter City Name"
+            solo
+          ></v-text-field>
+        </v-flex>
+      </v-form>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      city: 'London',
+      weather: {}
+    }
+  },
+  created() {},
+  methods: {
+    getWeatherInfo() {
+      this.$axios
+        .$get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${
+            this.city
+          }&appid=adaec12c8102f56ae4cbb886c5060f65`
+        )
+        .then(res => (this.weather = res))
+    }
+  }
+}
+</script>
+
+<style></style>
